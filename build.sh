28f56cfc1e2bef85eb8f66eeb0b64d89fb884a1a
Alessio Sanfratello <sanfra90@gmail.com>
2020-12-31 16:22:44 +0100

    Add --strip option to build

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ echo "Build docker image"
 docker build . -t $DOCKER_IMAGE
 
 echo "Compile project"
-docker run --rm -it -v $(pwd):/io $DOCKER_IMAGE build --release --manylinux off
+docker run --rm -it -v $(pwd):/io $DOCKER_IMAGE build --release --strip --manylinux off
 
 echo "Run auditwheel repair"
 docker run --rm -it -v $(pwd):/io --entrypoint /usr/bin/env $DOCKER_IMAGE auditwheel repair target/wheels/iterframes-*-linux_x86_64.whl
